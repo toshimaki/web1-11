00f81e38b6ec6a342dceedcf193216a922eba405
toshimaki <toshimaki@users.noreply.github.com>
2017-06-19 17:09:00 +0900

    Create default.js

---
+++ js/default.js
@@ -0,0 +1,16 @@
+var number = Math.floor(Math.random()*3);
+
+var answer = parseInt(window.prompt('数あてゲーム。０～２の数字を入力'));
+
+var message;
+if(answer === number){
+message = '正解';
+}else if (answer < number){
+message = '不正解、もっと大きいです';
+}else if (number < answer){
+message = '不正解、もっと小さいです';
+}else{
+message = '０～３の数字を入力してください';
+}
+
+document.getElementById('choice').textContent = message;
